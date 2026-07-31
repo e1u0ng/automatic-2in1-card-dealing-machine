7e1b9fe1117640364503ed4a01a6820005e47416
Ethan Luong <ethjjluong@gmail.com>
2026-07-30 17:59:10 -0700

    card shuffler v1 cad files and test code

--- main/servo_test.c
+++ main/servo_test.c
@@ -6,21 +6,21 @@
 // Hardware Definitions
 #define SERVO_GPIO              33           // GPIO pin connected to the goBILDA injector signal
 #define LEDC_TIMER              LEDC_TIMER_0
-#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_MODE               LEDC_HIGH_SPEED_MODE
 #define LEDC_OUTPUT_IO          SERVO_GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
 #define LEDC_DUTY_RES           LEDC_TIMER_14_BIT // 14-bit resolution (0 to 16383)
 #define LEDC_FREQUENCY          50           // 50 Hz PWM frequency (20ms period)
 
 // 14-bit duty values @ 50 Hz for 500us - 2500us pulse range
-#define SERVO_MIN_PULSE_DUTY    410          // 500 us pulse (0 degrees)
-#define SERVO_MID_PULSE_DUTY    1229         // 1500 us pulse (90 degrees)
-#define SERVO_MAX_PULSE_DUTY    2048         // 2500 us pulse (180 degrees)
+//#define SERVO_MIN_PULSE_DUTY    410          // 500 us pulse (0 degrees)
+//#define SERVO_MID_PULSE_DUTY    1229         // 1500 us pulse (90 degrees)
+//#define SERVO_MAX_PULSE_DUTY    2048         // 2500 us pulse (180 degrees)
 
 // 14-bit duty values @ 50 Hz for 1000us - 200us pulse range
-//#define SERVO_MIN_PULSE_DUTY    819          // 1000 us pulse (115 RPM CCW)
-//#define SERVO_MID_PULSE_DUTY    1229         // 1500 us pulse (0 RPM)
-//#define SERVO_MAX_PULSE_DUTY    1638         // 2000 us pulse (115 RPM CW)
+#define SERVO_MIN_PULSE_DUTY    819          // 1000 us pulse (115 RPM CCW)
+#define SERVO_MID_PULSE_DUTY    1229         // 1500 us pulse (0 RPM)
+#define SERVO_MAX_PULSE_DUTY    1638         // 2000 us pulse (115 RPM CW)
 
 void init_servo_pwm(void)
 {
@@ -84,24 +84,24 @@ void app_main(void)
     printf("Initializing Servo PWM on GPIO %d...\n", SERVO_GPIO);
     init_servo_pwm();
 
-    //set_servo_duty_2(1638);
+    set_servo_duty_1(SERVO_MAX_PULSE_DUTY);
+    set_servo_duty_2(SERVO_MAX_PULSE_DUTY);
+    vTaskDelay(pdMS_TO_TICKS(10000));
 
-    for (int i = 0; i < 3; i++) {
-        printf("Moving Servo to 180 Degrees (Max Pulse)\n");
+    /*for (int i = 0; i < 52; i++) {
+
         set_servo_duty_1(SERVO_MAX_PULSE_DUTY);
-        vTaskDelay(pdMS_TO_TICKS(1500));
+        vTaskDelay(pdMS_TO_TICKS(500));
 
-        printf("Moving Servo to 0 Degrees (Min Pulse)\n");
-        set_servo_duty_1(1500);
-        vTaskDelay(pdMS_TO_TICKS(1500));
+        
+        set_servo_duty_1(1750);
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }*/
 
-        //printf("Moving Servo to 90 Degrees (Mid Pulse)\n");
-        //set_servo_duty(SERVO_MID_PULSE_DUTY);
-        //vTaskDelay(pdMS_TO_TICKS(1500));
-    }
-
-    //set_servo_duty_2(1229);
+    set_servo_duty_1(SERVO_MID_PULSE_DUTY);
+    set_servo_duty_2(SERVO_MID_PULSE_DUTY);
 
     ledc_stop(LEDC_MODE, LEDC_CHANNEL, 0);    
+    ledc_stop(LEDC_MODE, LEDC_CHANNEL_1, 0);  
 }
 
